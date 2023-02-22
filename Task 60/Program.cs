4b08f0925f73bb5dda585182f9b6b673bfee44cc
ElenaKulikova <elena_komb@mail.ru>
2023-02-22 12:31:41 +0200

    Added to solution to Task 60

---
+++ Task 60/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+// Функция вывода индекса элементов 3D массива
+void PrintIndex(int[,,] matrix3D)            
+{
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                Console.Write($"{matrix3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+// Функция заполнения 3D массива неповторяющимися числами
+void GenerateMatrix(int[,,] matrix)
+{
+    int count = 10;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+
+int[,,] matrix3D = new int[2, 2, 2];
+GenerateMatrix(matrix3D);
+PrintIndex(matrix3D);
+
+
+
+
+
