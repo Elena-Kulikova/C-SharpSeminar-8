9d8090cec608d87da427d55914123ead1d62ba56
ElenaKulikova <elena_komb@mail.ru>
2023-02-22 11:46:45 +0200

    Added solution to Task 58

---
+++ Task 58/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+int[,] GenerateMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] product = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            product[i, j] = matrix1[i, j] * matrix2[i, j];
+        }
+    }
+    return product;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //cols
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+    
+}
+
+int row = 3;
+int col = 5;
+
+int[,] matrix1 = GenerateMatrix(row, col, 1, 10);
+int[,] matrix2 = GenerateMatrix(row, col, 10, 20);
+int[,] product = MultiplyMatrix(matrix1, matrix2);
+PrintMatrix(matrix1);
+System.Console.WriteLine("----------------------------------");
+PrintMatrix(matrix2);
+System.Console.WriteLine("----------------------------------");
+PrintMatrix(product);
