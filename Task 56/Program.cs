3271407e22a7bc30dcd71f98a01e65ecbe9ed05a
ElenaKulikova <elena_komb@mail.ru>
2023-02-22 11:26:51 +0200

    Added solution to Task 56

---
+++ Task 56/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //cols
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //cols
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+
+void NumberRowMinSumElements(int[,] matrix)
+{
+    int minRow = 0;
+    int sumRow = 0;
+    int minSumRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minRow += matrix[0, i];
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) sumRow += matrix[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"Номер строки с наименьшей суммой элементов: {minSumRow + 1} строка");
+}
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int cols = ReadInt("Введите количество столбцов  матрицы: ");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+Console.WriteLine();
+NumberRowMinSumElements(myMatrix);
